d00fb634410d9c7c41b82121221d35a6380426ef
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2022-03-05 17:04:44 +0300

    added grid layout

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,7 +1,18 @@
 import cn from 'classnames';
 import { FooterProps } from './Footer.props';
-import styles from './Sidebar.module.css';
+import styles from './Footer.module.css';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div className='footer-item'>OwlTop &#169; 2020 &#8211; {format(new Date(), 'yyyy')} Все права защищены</div>
+      <a href='#' target='_blank'>
+        Пользовательское соглашение
+      </a>
+      <a href='#' target='_blank'>
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
