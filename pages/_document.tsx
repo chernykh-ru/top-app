2f786755ffb314338e69e0101bae5dd3bb8fed83
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2022-03-09 15:35:31 +0300

    added SSR

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,13 @@ class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang='ru '>
-        <Head />
+        <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300;400;500;700&display=swap'
+            rel='stylesheet'></link>
+        </Head>
         <body>
           <Main />
           <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
+import axios from 'axios';
+import { GetStaticProps } from 'next';
 import { Button, Htag, P, Tag, Rating } from '../components';
 import { withLayout } from '../layout/Layout';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(0);
 
   return (
@@ -40,8 +43,31 @@ function Home(): JSX.Element {
       </Tag>
       <Rating rating={rating} isEditable={true} setRating={setRating} />
       <Rating rating={3} />
+      <ul>
+        {menu.map((m) => (
+          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+        ))}
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory,
+  });
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
