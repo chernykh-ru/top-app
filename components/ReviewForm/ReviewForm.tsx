6c642baf8e10c56712b119c7a7dccab38f462dda
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2022-03-21 15:01:32 +0300

    use review form with react-hook-form

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -6,18 +6,32 @@ import { Input } from '../Input/Input';
 import { Textarea } from '../Textarea/Textarea';
 import { Button } from '../Button/Button';
 import CloseIcon from './close.svg';
+import { useForm, Controller } from 'react-hook-form';
+import { IReviewForm } from './ReviewForm.interface';
 
 export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps): JSX.Element => {
+  const { register, control, handleSubmit } = useForm<IReviewForm>();
+
+  const onSubmit = (data: IReviewForm) => {
+    console.log(data);
+  };
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className={cn(styles.reviewForm, className)} {...props}>
-        <Input placeholder='Имя' />
-        <Input className={styles.title} placeholder='Заголовок отзыва' />
+        <Input {...register('name')} placeholder='Имя' />
+        <Input {...register('title')} className={styles.title} placeholder='Заголовок отзыва' />
         <div className={styles.rating}>
           <span>Оценка:</span>
-          <Rating rating={0} isEditable={true} />
+          <Controller
+            control={control}
+            name={'rating'}
+            render={({ field }) => (
+              <Rating rating={field.value} isEditable setRating={field.onChange} ref={field.ref} />
+            )}
+          />
         </div>
-        <Textarea className={styles.description} placeholder='Текст отзыва' />
+        <Textarea {...register('description')} className={styles.description} placeholder='Текст отзыва' />
         <div className={styles.submit}>
           <Button appearance='primary'>Отправить</Button>
           <span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
@@ -28,6 +42,6 @@ export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps):
         <div className={styles.successDescription}>Спасибо, ваш отзыв будет опубликован после проверки.</div>
         <CloseIcon className={styles.close} />
       </div>
-    </>
+    </form>
   );
 };
