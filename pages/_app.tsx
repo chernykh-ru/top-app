83545027e3edc11110f180fc6518eb47401d9fc5
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2022-03-28 11:19:49 +0300

    added yandex metrica

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import Head from 'next/head';
+import Router from 'next/router';
 import '../styles/globals.css';
+import ym from 'react-yandex-metrika';
+import { YMInitializer } from 'react-yandex-metrika';
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
+  Router.events.on('routeChangeComplete', (url: string) => {
+    if (typeof window !== undefined) {
+      ym('hit', url);
+    }
+  });
   return (
     <>
       <Head>
@@ -11,7 +19,9 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
         <meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
         <meta property='og:locale' content='ru_RU' />
         <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://mc.yandex.ru' />
       </Head>
+      <YMInitializer accounts={[]} options={{ webvisor: true, defer: true }} version='2' />
       <Component {...pageProps} />
     </>
   );
